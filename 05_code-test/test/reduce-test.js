b144cf7701a9d69089d20d083e2344c485de7169
HeeHeeHee <praaa0220@gmail.com>
2023-02-20 15:58:02 +0900

    sass 연습

--- 05_code-test/test/reduce-test.js
+++ 05_code-test/test/reduce-test.js
@@ -1,8 +1,14 @@
-let numbers = [];
-for (let i = 1; i <= 100; i++) {
-  numbers.push(i);
+// let numbers = [1, 2, 3, 3, 3];
+
+// const numbers_sum = numbers.reduce((acc, item) => (acc += item), 0);
+
+// console.log(numbers_sum / numbers.length);
+
+function solution(numbers) {
+  let answer = numbers.reduce((acc, item) => {
+    return (acc += item);
+  }, 0);
+  return answer / numbers.length;
 }
 
-const numbers_sum = numbers.reduce((acc, item) => (acc += item), 0);
-
-console.log(numbers_sum);
+console.log(solution([1, 2, 3]));
